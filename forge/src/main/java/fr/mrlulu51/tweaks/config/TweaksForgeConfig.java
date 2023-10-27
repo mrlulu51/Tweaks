98990647f3ea086087462799181027a7fa641c6f
mrlulu51 <lulubinou51@gmail.com>
2023-10-27 10:00:47 +0200

    Created ForgeConfig Map

--- forge/src/main/java/fr/mrlulu51/tweaks/config/TweaksForgeConfig.java
+++ forge/src/main/java/fr/mrlulu51/tweaks/config/TweaksForgeConfig.java
@@ -4,18 +4,32 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TweaksForgeConfig {
     public final ForgeConfigSpec.BooleanValue shulkerViewerEnabled;
     public final ForgeConfigSpec.BooleanValue entityLifeDisplayEnabled;
 
+    public final Map<String, ForgeConfigSpec.ConfigValue<?>> configValues = new HashMap<>();
+    public final ForgeConfigSpec.Builder builder;
+
     public TweaksForgeConfig(ForgeConfigSpec.Builder builder) {
-        shulkerViewerEnabled = builder.define("shulkerViewEnabled", true);
-        entityLifeDisplayEnabled = builder.define("entityLifeDisplayEnabled", true);
+        this.builder = builder;
+
+        this.shulkerViewerEnabled = this.addBooleanConfigValue("shulker_view", true);
+        this.entityLifeDisplayEnabled = this.addBooleanConfigValue("entity_life_display", true);
     }
 
     public static TweaksForgeConfig registerConfigSpecs(ModLoadingContext ctx) {
         var spec = new ForgeConfigSpec.Builder().configure(TweaksForgeConfig::new);
         ctx.registerConfig(ModConfig.Type.CLIENT, spec.getRight());
         return spec.getLeft();
+    }
+
+    public <C extends ForgeConfigSpec.ConfigValue<Boolean>> C addBooleanConfigValue(String path, boolean value) {
+        C configType = (C) this.builder.define(path, value);
+        configValues.put(path, configType);
+        return configType;
     }
 }
